2bc3e54ad11313f612ce6a0e048e900e9a3c9fdf
Llewellyn Collins <llewellyn.c.collins@gmail.com>
2021-08-08 21:48:39 +0100

    chore: add material files

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import BaseNav from "./components/BaseNav.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { BaseNav },
+});
+</script>
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">Pearls of Wisdom</span>
+      </div>
+    </header>
+    <base-nav />
+    <main class="mdl-layout__content">
+      <router-view />
+    </main>
   </div>
-  <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.mdl-layout__drawer-button {
+  margin: 4px;
+  color: rgba(0, 0, 0, 0.5);
+}
+</style>
